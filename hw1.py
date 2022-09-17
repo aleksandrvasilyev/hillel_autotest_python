4e4f787a6b2375de77fc99db089f397cb6bec8e0
aleksandrvasilyev <saddon11@gmail.com>
2022-09-17 20:07:17 +0300

    first commit

---
+++ hw1.py
@@ -0,0 +1,24 @@
+first = 10
+second = 30
+
+print(first + second)
+print(first - second)
+print(first * second)
+print(first ** second)
+print(first / second)
+print(first // second)
+print(first % second)
+
+
+result = first < second
+print(result)
+
+result = first > second
+print(result)
+
+result = first == second
+print(result)
+
+result = first != second
+print(result)
+
