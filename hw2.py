a7c527e567b50a5698b3e1358957fcc24adaafa7
aleksandrvasilyev <saddon11@gmail.com>
2022-09-26 14:13:43 +0300

    edit homework 2

--- hw2.py
+++ hw2.py
@@ -1,18 +1,21 @@
 print('Касир в кінотеватрі')
 
-age = input('Ваш вік: ')
+while True:
+    age = input('Ваш вік: ')
 
-if age.isdigit():
-    age = int(age)
-    if age < 7:
-        print('Де твої батьки?')
-    elif age < 16:
-        print('Це фільм для дорослих!')
-    elif age > 65:
-        print('Покажіть пенсійне посвідчення!')
-    elif '7' in str(age):
-        print('Вам сьогодні пощастить!')
+    if age.isdigit():
+        age_int = int(age)
+        if age_int < 1:
+            print('Вы еще не родились')
+        elif age_int < 7:
+            print('Де твої батьки?')
+        elif age_int < 16:
+            print('Це фільм для дорослих!')
+        elif age_int > 65:
+            print('Покажіть пенсійне посвідчення!')
+        elif '7' in age:
+            print('Вам сьогодні пощастить!')
+        else:
+            print('А білетів вже немає!')
     else:
-        print('А білетів вже немає!')
-else:
-    print('Введіть вік')
+        print('Введіть корректний вік')
