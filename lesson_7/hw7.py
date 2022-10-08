b35f4647936e80d81d6e16d8f4259bf998ea0c75
aleksandrvasilyev <saddon11@gmail.com>
2022-10-08 14:46:24 +0300

    edit hw7

--- lesson_7/hw7.py
+++ lesson_7/hw7.py
@@ -1,7 +1,7 @@
 print('Касир в кінотеатрі')
 
 
-def sklonenie(age):
+def declination(age):
     age = int(age)
     if age % 10 == 1 and age != 11 and age % 100 != 11:
         return 'рік'
@@ -16,16 +16,16 @@ def know_age(age):
         age_int = int(age)
         if age_int < 1:
             print('Вы еще не родились')
-        elif age_int < 7:
-            print(f'Тобі ж {age} {sklonenie(age)}! Де твої батьки?')
-        elif age_int < 16:
-            print(f'Тобі лише {age} {sklonenie(age)}, а це е фільм для дорослих!')
-        elif age_int > 65:
-            print(f'Вам {age} {sklonenie(age)}? Покажіть пенсійне посвідчення!')
         elif '7' in age:
-            print(f'Вам {age} {sklonenie(age)}, вам пощастить')
+            print(f'Вам {age} {declination(age)}, вам пощастить')
+        elif age_int < 7:
+            print(f'Тобі ж {age} {declination(age)}! Де твої батьки?')
+        elif age_int < 16:
+            print(f'Тобі лише {age} {declination(age)}, а це е фільм для дорослих!')
+        elif age_int > 65:
+            print(f'Вам {age} {declination(age)}? Покажіть пенсійне посвідчення!')
         else:
-            print(f'Незважаючи на те, що вам {age} {sklonenie(age)}, білетів всеодно нема!')
+            print(f'Незважаючи на те, що вам {age} {declination(age)}, білетів всеодно нема!')
     else:
         print('Введіть корректний вік')
 
