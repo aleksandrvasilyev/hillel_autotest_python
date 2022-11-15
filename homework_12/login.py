76bf9cf8cb9963cd4edc2908e0a991ef3725dd09
aleksandrvasilyev <saddon11@gmail.com>
2022-11-15 19:02:33 +0100

    edited

--- homework_12/login.py
+++ homework_12/login.py
@@ -1,16 +1,19 @@
 import time
-
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_01():
-    chrome_driver = Chrome()
+    chrome_driver = Chrome(service=Service(ChromeDriverManager().install()))
     chrome_driver.maximize_window()
     chrome_driver.get('https://demoqa.com/login')
-    chrome_driver.find_element(By.XPATH, '//input[@id="userName"]').send_keys('aleksandr111')
+    login = 'aleksandr111'
+    password = '7lB2Tw%Hxi_B'
+    chrome_driver.find_element(By.XPATH, '//input[@id="userName"]').send_keys(login)
     time.sleep(2)
-    chrome_driver.find_element(By.XPATH, '//input[@id="password"]').send_keys('7lB2Tw%Hxi_B')
+    chrome_driver.find_element(By.XPATH, '//input[@id="password"]').send_keys(password)
     time.sleep(2)
     chrome_driver.find_element(By.XPATH, '//button[@id="login"]').click()
     time.sleep(3)
